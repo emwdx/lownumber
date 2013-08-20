f4ea42e8b059d792e366dad813e99012c2a1bf8c
Evan Weinberg <weinbergmath@gmail.com>
2013-08-20 21:12:03 +0800

    Structure update

--- lownumber.js
+++ lownumber.js
@@ -1,94 +1,140 @@
 
 
+LowNumbers = new Meteor.Collection('lownumbers');
 
-Results = new Meteor.Collection('results');
 
+Router.map(function() { 
+	this.route('entry',{path: '/'});
+    this.route('resultsList', {
+    path: '/results/',
+    data: function(){
+    allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+    return allResults[0];
+    }
+    });
+    
+    this.route('waiting',{path: '/waiting/'});
+    this.route('config',{path: '/config/'});
+    
+  
+  });
 
 
 if (Meteor.isClient) {
- 
 Template.resultsList.helpers({
 
 	results: function(){
-		allResults = Results.find({},{sort: {number: 1}})
-		arrayResults = allResults.fetch();
-		
-		lastNumber = 0;
-		for(var i=0;i<arrayResults.length;i++){
-			if(arrayResults[i].number==lastNumber){
-				arrayResults[i].winClass = 'same'
-			}
-			lastNumber = arrayResults[i].number
-		
-		}
-		
-		return allResults;
+	allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+	currentID =allResults.length-1
+	guesses = LowNumbers.findOne({classID: currentID}).guesses
+	
+	guesses.sort(function(a,b) { return parseFloat(a.number) - parseFloat(b.number) } );
+	
+	for (var i=0; i < guesses.length-1; i++) {
+      if (guesses[i].number === guesses[i+1].number) {
+        guesses[i].winClass = "same"
+        guesses[i+1].winClass = "same"
+        }
+    }
+
+    // Check if we have a winner
+    // It should be the first index, and shouldn't have a class yet
+    
+    
+    for (var i=0; i < guesses.length-1; i++) {
+      if (guesses[i].winClass != "same") {
+        guesses[i].winClass = "success"
+        break;
+        
+      }
+      
+    }
+    	
+	return guesses	
+	}
+
+
+});
+Template.waiting.helpers({
+	lowNumber: function(){
+	allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+	currentID =allResults.length-1
+	return LowNumbers.findOne({classID: currentID})
+	
+	}
+
+
+});
+
+Template.config.events(
+	{'click #newGame':function(){
+	allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+	newLowNumber = {
+	classID: allResults.length,
+	finished: false,
+	guesses:[]
+	
+	},
+	
+	LowNumbers.insert(newLowNumber);
+	Router.go('/');
+
+	},
+	'click #endGame':function(){
+	allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+	currentID =allResults.length-1
+	currentGame = LowNumbers.findOne({classID: currentID})._id
+	LowNumbers.update({_id: currentGame},
+                   {$set: {finished: true}});
+	
+	Router.go('/');
+	
 	}
 	
+	});
 	
 	
-});
-Meteor.Router.add({
-	'/': 'entry',
-	'/waiting/': 'waiting',
-	'/results/':'resultsList'
-	});	
-
+	
 Template.entry.events({ 'submit form': function(e) {
 	e.preventDefault();
-	var result = {
+	var newGuess = {
 				number: parseInt($(e.target).find('[name=myNumber]').val()),
 				name: $(e.target).find('[name=myName]').val(), 
-				winClass: ''
-
-	}
-	Results.insert(result);
-	
-  	Meteor.Router.to('/waiting/');
+				winClass: '',
+    };
+	allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+	currentID = allResults.length-1;
+	currentGame = LowNumbers.findOne({classID: currentID})._id
+	LowNumbers.update({_id: currentGame}, 
+	{$push:{guesses:newGuess}});
+  	Router.go('/waiting/');
 }
 });
 
 
-
-Template.resultsList.events({'click td': function(event){
-	if($(event.target).parent().parent().hasClass('same')){
-		
-	$(event.target).parent().parent().removeClass('same')}
-	else{
-	$(event.target).parent().parent().addClass('same')
-	}
-	
-
-
-
 }
 
-});
-
-
-}
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if(Results.find().count()===0){
-    Results.insert({ number: 10,
-    name: 'Evan'
-    });
-    Results.insert({ number: 4,
-    name: 'Mileaux'
-   
-    });
-    Results.insert({ number: 4,
-    name: 'Josie'
-   
-    });
-    Results.insert({ number: 8,
-    name: 'Micah'
-   
-    });
-    }
-   
-  });
+allResults = LowNumbers.find({},{sort: {classID: 1}}).fetch();
+currentID =allResults.length-1
+if(currentID==-1){
+newLowNumber = {
+	classID: allResults.length,
+	finished: false,
+	guesses:[]
+	
+	},
+	
+	LowNumbers.insert(newLowNumber);
+
 }
+
+
+
+}
+
+
+
 
 
